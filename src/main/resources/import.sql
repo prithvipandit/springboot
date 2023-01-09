d00633d40133457961100912cd366a761f6ffb53
Piyush Kumar <69364167+prithvipandit@users.noreply.github.com>
2023-01-09 19:26:40 +0530

    Create import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+insert into Librarian values('librarian501@gmail.com','libone');
+insert into Librarian values('librarian502@gmail.com','libtwo');
+
+insert into Author values(21,'William S.');
+insert into Author values(23,'William W.');
+insert into Author values(22,'R.D. Sharma');
+insert into Author values(24,'H.C. Verma');
+
+insert into Book values(501,21,'The Tale Of Spring','William S.');
+insert into Book values(511,23,'Spring Season','William S.');
+insert into Book values(523,22,'Introduction to Maths','R.D. Sharma');
+insert into Book values(421,24,'Concept of Physics','H.C. Verma');
